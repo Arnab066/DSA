3c177eceb2beef375e52ceb9ff9a257db4ca9df2
Arnab Chowdhury <chowdhuryarnab066@gmail.com>
2026-07-30 23:15:08 +0530

    code

--- Array_II/Search_in_sorted_matrix.java
+++ Array_II/Search_in_sorted_matrix.java
@@ -1,5 +1,22 @@
 package Array_II;
 
-public class Search_in_sorted_matrix {
-    
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0, right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int mid_val = matrix[mid / n][mid % n];
+
+            if (mid_val == target)
+                return true;
+            else if (mid_val < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+    }
 }
