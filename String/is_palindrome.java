b1c9fb35b1c6c21288e65309266da928aa7f5ff9
Arnab Chowdhury <chowdhuryarnab066@gmail.com>
2026-07-31 22:57:01 +0530

    Update is_palindrome.java with commented LeetCode 125 solution

--- String/is_palindrome.java
+++ String/is_palindrome.java
@@ -6,9 +6,41 @@ class is_palindrome {
         for (int i = 0; i < s.length() / 2; i++) {
             if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
                 System.out.println("False");
-                break;
+                return;
             }
         }
         System.out.println("True");
     }
 }
+/*
+ * //leetcode 125
+ * 
+ * class Solution {
+ * public boolean isPalindrome(String s) {
+ * 
+ * int left = 0;
+ * int right = s.length() - 1;
+ * 
+ * while (left < right) {
+ * 
+ * while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+ * left++;
+ * }
+ * 
+ * while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+ * right--;
+ * }
+ * 
+ * if (Character.toLowerCase(s.charAt(left))
+ * != Character.toLowerCase(s.charAt(right))) {
+ * return false;
+ * }
+ * 
+ * left++;
+ * right--;
+ * }
+ * 
+ * return true;
+ * }
+ * }
+ */
