d277535e3cac6598f24ff1d41253160375fdd9e8
Arnab Chowdhury <chowdhuryarnab066@gmail.com>
2026-08-05 23:53:45 +0530

    Update test input for median_of_two_sorted_arrays

--- Array_II/median_of_two_sorted_arrays.java
+++ Array_II/median_of_two_sorted_arrays.java
@@ -1,16 +1,15 @@
 package Array_II;
+
 public class median_of_two_sorted_arrays {
     public static void main(String[] args) {
-        int[] nums1 = { 1, 2 , 3 , 4 , 5};
+        int[] nums1 = { 1, 2 , 3 , 4, 5, 6, 7 , 8 , 9 ,10 };
         int[] nums2 = { 3, 4 };
-
         int m = nums1.length;
         int n = nums2.length;
 
         int[] arr = new int[m + n];
 
         int i = 0, j = 0, k = 0;
-
         while (i < m && j < n) {
             if (nums1[i] < nums2[j])
                 arr[k++] = nums1[i++];
